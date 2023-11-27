327bb0cc933218bc036381d8f60ed746e090c6a4
Vojta Oupicky <voj.oup@gmail.com>
2023-11-26 21:37:12 -0500

    Initial commit

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,30 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  keys = {
+    {
+      "<leader>?",
+      function()
+        require("telescope.builtin").oldfiles()
+      end,
+      desc = "Find Recently open files",
+    },
+    -- change a keymap
+    {
+      "<leader>/",
+      function()
+        require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+          winblend = 10,
+          previewer = false,
+        }))
+      end,
+      desc = "Find in buffer",
+    },
+    {
+      "<leader>sr",
+      function()
+        require("telescope.builtin").resume()
+      end,
+      desc = "Resume last search",
+    },
+  },
+}
